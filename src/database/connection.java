165acd3489d596d9e9b369e91f7ece2d0584171c
renfred <renfred.19@ichat.sp.edu.sg>
2020-07-30 17:14:44 +0800

    Added src/database to reduce repetitive code

---
+++ src/database/connection.java
@@ -0,0 +1,17 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connection {
+
+	public static Connection connectToDatabase() throws SQLException, ClassNotFoundException {
+		// connect to database
+		Class.forName("com.mysql.jdbc.Driver");
+		String connURL = "jdbc:mysql://localhost/jad?user=root&password=password&serverTimezone=UTC";
+		Connection conn = DriverManager.getConnection(connURL);
+		
+		return conn;
+	}
+}
