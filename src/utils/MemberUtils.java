cd3f0db396f6aee393d34e9a583a416919e13a54
renfred <renfred.19@ichat.sp.edu.sg>
2020-07-30 23:15:22 +0800

    Complete utils/MemberUtils.java

--- src/utils/MemberUtils.java
+++ src/utils/MemberUtils.java
@@ -1,110 +1,120 @@
 package utils;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import database.Database;
 import javabeans.Member;
 
 public class MemberUtils {
-	public Member getMembers (int userId) {
-		Member uBean = null;
-		Connection conn = null;
+	
+	// get all members
+	public ArrayList<Member> getMembers () throws Exception {
+		// connect to database
+		Connection conn = Database.connectToDatabase();
 		
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/duotexture?user=root&password=password&serverTimezone=UTC");
-			Statement stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM duotexture.members WHERE userId =" + userId);
+		// statement, get all members query and results
+		Statement stmt = conn.createStatement();
+		String getMembersQuery = "SELECT * FROM duotexture.members;";
+		ResultSet getMembersResult = stmt.executeQuery(getMembersQuery);
+		
+		// create new ArrayList of Members
+		ArrayList<Member> membersArrayList = new ArrayList<Member>();
+		
+		// loop if there are new row
+		while(getMembersResult.next()) {
+			// create an instance of Member
+			Member MemberBean = new Member();
 			
-			if(rs.next()) {
-				uBean = new Member();
-				uBean.setFirstName(rs.getString("first_name"));
-				uBean.setLastName(rs.getString("last_name"));
-				uBean.setCountry(rs.getString("country"));
-				uBean.setAddress(rs.getString("address"));
-				uBean.setPostalCode(rs.getInt("postal_code"));
-				uBean.setUserId(rs.getInt("userId"));
-			}
-		}catch(Exception e) {
-		}finally {
-			try {
-				conn.close();
-			}catch(Exception e) {}
+			MemberBean.setUserId(getMembersResult.getInt("userId"));
+			MemberBean.setFirstName(getMembersResult.getString("first_name"));
+			MemberBean.setLastName(getMembersResult.getString("last_name"));
+			MemberBean.setCountry(getMembersResult.getString("country"));
+			MemberBean.setAddress(getMembersResult.getString("address"));
+			MemberBean.setPostalCode(getMembersResult.getInt("postal_code"));
+			
+			// add MembersBean to categoriesArrayList
+			membersArrayList.add(MemberBean);
 		}
-		return uBean;
+		
+		// close connection
+		conn.close();
+		return membersArrayList;
 	}
 	
-	public int insertMembers (String firstName, String lastName, String country, String address, int postalCode) {
-		Connection conn = null;
-		int count = 0;
+	// get member by id
+	public Member getMemberById (int userId) throws ClassNotFoundException, SQLException {
+		// connect to database
+		Connection conn = Database.connectToDatabase();
 		
-		try {
-			// connect to mysql database
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/duotexture?user=root&password=password&serverTimezone=UTC");
-			Statement stmt = conn.createStatement(); 
-			
-			// get last row of users
-			String getLastUserQuery = "SELECT * FROM users ORDER BY userId DESC LIMIT 1;";
-			ResultSet getLastUserResult = stmt.executeQuery(getLastUserQuery);
-			
-			getLastUserResult.next();
-			int userId = getLastUserResult.getInt("userId");
-			String memberUsername = getLastUserResult.getString("username");
-			
-//			session.setAttribute("userId", userId);
-//			session.setAttribute("username", memberUsername);
-//			session.setAttribute("accountType", "member");
-			
-			// insert inputs into members table
-			String insertMemberQuery = "INSERT INTO members(`first_name`, `last_name`, `country`, `address`, `postal_code`, `userId`) VALUES(?, ?, ?, ?, ?, ?);";
-			PreparedStatement pstmt = conn.prepareStatement(insertMemberQuery);
-		    pstmt.setString(1, firstName);
-		    pstmt.setString(2, lastName);
-		    pstmt.setString(3, country);
-		    pstmt.setString(4, address);
-		    pstmt.setInt(5, postalCode);
-		    pstmt.setInt(6, userId);
-			count = pstmt.executeUpdate(); 
-			
-		}catch(Exception e) {
-		}finally {
-			try {
-				conn.close();
-			}catch(Exception e) {}
+		// prepared statement, get member by id query and results
+		String getMemberByIdQuery = "SELECT * FROM duotexture.members WHERE userId=?;";
+		PreparedStatement pstmt = conn.prepareStatement(getMemberByIdQuery);
+		pstmt.setInt(1,  userId);
+		ResultSet getMemberByIdResult = pstmt.executeQuery();
+		
+		// create an instance of member
+		Member memberBean = new Member();
+		
+		// if there is a new row
+		if(getMemberByIdResult.next()) {
+			memberBean.setFirstName(getMemberByIdResult.getString("first_name"));
+			memberBean.setLastName(getMemberByIdResult.getString("last_name"));
+			memberBean.setCountry(getMemberByIdResult.getString("country"));
+			memberBean.setAddress(getMemberByIdResult.getString("address"));
+			memberBean.setPostalCode(getMemberByIdResult.getInt("postal_code"));
+			memberBean.setUserId(getMemberByIdResult.getInt("userId"));
 		}
+	
+		// close connection
+		conn.close();
+		return memberBean;
+	}
+	
+	// add members
+	public int insertMember (String firstName, String lastName, String country, String address, int postalCode, int userId) throws SQLException, ClassNotFoundException {
+
+		// connect to database
+		Connection conn = Database.connectToDatabase();
+		
+		// prepared statement, add members query and results
+		String insertMemberQuery = "INSERT INTO members(`first_name`, `last_name`, `country`, `address`, `postal_code`, `userId`) VALUES(?, ?, ?, ?, ?, ?);";
+		PreparedStatement pstmt = conn.prepareStatement(insertMemberQuery);
+	    pstmt.setString(1, firstName);
+	    pstmt.setString(2, lastName);
+	    pstmt.setString(3, country);
+	    pstmt.setString(4, address);
+	    pstmt.setInt(5, postalCode);
+	    pstmt.setInt(6, userId);
+		int count = pstmt.executeUpdate(); 
+		
+		// close connection
+		conn.close();
 		return count;
 	}
 	
-	public int editMembers (String firstName, String lastName, String country, String address, int postalCode, int userId) {
-		Connection conn = null;
-		int count = 0;
+	// edit member
+	public int editMember (String firstName, String lastName, String country, String address, int postalCode, int userId) throws SQLException, ClassNotFoundException {
+		// connect to database
+		Connection conn = Database.connectToDatabase();
 		
-		try {
-			// connect to mysql database
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection("jdbc:mysql://localhost/duotexture?user=root&password=password&serverTimezone=UTC");
-
-			// edit and update members with inputs by user id
-			String updateMembersQuery = "UPDATE members SET first_name=?, last_name=?, country=?, address=?, postal_code=? WHERE userId=?;"; 
-			PreparedStatement pstmt = conn.prepareStatement(updateMembersQuery);
-			pstmt.setString(1, firstName);
-		    pstmt.setString(2, lastName);
-		    pstmt.setString(3, country);
-		    pstmt.setString(4, address);
-		    pstmt.setInt(5, postalCode);
-		    pstmt.setInt(6, userId);
-		    count = pstmt.executeUpdate();
-			
-		}catch(Exception e) {
-		}finally {
-			try {
-				conn.close();
-			}catch(Exception e) {}
-		}
+		// prepared statement, edit members query and result
+		String updateMembersQuery = "UPDATE members SET first_name=?, last_name=?, country=?, address=?, postal_code=? WHERE userId=?;"; 
+		PreparedStatement pstmt = conn.prepareStatement(updateMembersQuery);
+		pstmt.setString(1, firstName);
+	    pstmt.setString(2, lastName);
+	    pstmt.setString(3, country);
+	    pstmt.setString(4, address);
+	    pstmt.setInt(5, postalCode);
+	    pstmt.setInt(6, userId);
+	    int count = pstmt.executeUpdate();
+		
+	    // close connection
+		conn.close();
 		return count;
 	}
 }
