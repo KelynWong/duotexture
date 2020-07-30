9e41b94e5b66ce810abed18c3d810f628b6b4bff
renfred <renfred.19@ichat.sp.edu.sg>
2020-07-31 02:16:33 +0800

    Completed SignOutServlet.java

--- src/servlets/SignOutServlet.java
+++ src/servlets/SignOutServlet.java
@@ -46,16 +46,15 @@ public class SignOutServlet extends HttpServlet {
 		// get writer
 		PrintWriter out = response.getWriter();
 		
+		// validate if user is logged in with an account type
 		if(session.getAttribute("accountType")!=null){			
 			// mark session invalid and destroy
 			session.invalidate();  
-			out.println("<script type='text/javascript'>");
-			out.println("window.location.href='Assignment/website/index.jsp';");
-			out.println("alert('Logged out.');");
-			out.println("</script>");
+			response.sendRedirect("Assignment/website/index.jsp");
+			System.out.println("(SignOutServlet) Logged out!");
 		} else {
 			out.println("<script type='text/javascript'>");
-			out.println("window.location.href='Assignment/website/index.jsp';");
+			out.println("window.location.href='Assignment/website/login.jsp';");
 			out.println("alert('You are not logged in.');");
 			out.println("</script>");
 		}
@@ -65,8 +64,26 @@ public class SignOutServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// doGet(request, response);
+		// get current session
+		HttpSession session=request.getSession();
+
+		// get writer
+		PrintWriter out = response.getWriter();  
+		
+		try{ 
+			// validate if user is logged in with an account type
+			if(session.getAttribute("accountType")!=null){
+				System.out.println("(SignOutServlet) There's no action to be taken for POST. Redirecting to index.jsp."); 
+				response.sendRedirect("Assignment/website/index.jsp");
+			} else{
+				out.println("<script type='text/javascript'>");
+				out.println("window.location.href='Assignment/website/login.jsp';");
+				out.println("alert('You are not logged in.');");
+				out.println("</script>");
+			}
+		} catch (Exception e){
+			System.out.println("(SignOutServlet) Admin Validation Error: " + e + "\n");
+		}
 	}
 
 }
