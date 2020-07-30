447d4719da0fbe17a60ad352a17b3b23608b07ae
renfred <renfred.19@ichat.sp.edu.sg>
2020-07-31 02:08:46 +0800

    Completed LoginServlet.java

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import java.sql.*;
+import javabeans.User;
+import utils.UserUtils;
+import utils.MemberUtils;
 
 /**
  * Servlet implementation class LoginServlet
@@ -41,71 +45,95 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//  response.getWriter().append("Served at: ").append(request.getContextPath());
+		// get current session
+		HttpSession session=request.getSession();
+
+		// get writer
+		PrintWriter out = response.getWriter();  
+		
+		try{ 
+			// validate if user is logged in with an account type
+			if(session.getAttribute("accountType")!=null){
+				System.out.println("(LoginServlet) There's no action to be taken for GET. Redirecting to index.jsp."); 
+				response.sendRedirect("Assignment/website/index.jsp");
+			} else{
+				out.println("<script type='text/javascript'>");
+				out.println("window.location.href='Assignment/website/login.jsp';");
+				out.println("alert('You are not logged in.');");
+				out.println("</script>");
+			}
+		} catch (Exception e){
+			System.out.println("(LoginServlet) Admin Validation Error: " + e + "\n");
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// doGet(request, response);
-		try{
-			if(request.getParameter("emailInput")!=null){
-				String emailInput = request.getParameter("emailInput");
-				String passwordInput = request.getParameter("passwordInput");
-				
-				Boolean isAdmin = false;
-				Boolean isMember = false;
-				
-				// get current session
-        		HttpSession session=request.getSession();
-				
-				// connect to mysql database
-				Class.forName("com.mysql.jdbc.Driver");
-				String connURL = "jdbc:mysql://localhost/duotexture?user=root&password=password&serverTimezone=UTC";
-				Connection conn = DriverManager.getConnection(connURL);
-				Statement stmt = conn.createStatement();
-				
-				// get all users
-				String getAllAdminsQuery = "SELECT * FROM users;";
-				ResultSet getAllUsersResult = stmt.executeQuery(getAllAdminsQuery);
-				
-				while (getAllUsersResult.next()){
-					String userEmail = getAllUsersResult.getString("email");
-					String userPassword = getAllUsersResult.getString("password");
-					String userRole = getAllUsersResult.getString("userRole");
-					
-					// checks if inputs equals details in users table
-					if(userEmail.equals(emailInput) && userPassword.equals(passwordInput)){
-						int userId = getAllUsersResult.getInt("userId");
-						String username = getAllUsersResult.getString("username");
-						if(userRole.equals("Admin")){
-							isAdmin = true;
-							session.setAttribute("accountType", "admin");
-						}else if(userRole.equals("Member")){
-							isMember = true;
-							session.setAttribute("accountType", "member");
+		// get current session
+		HttpSession session=request.getSession();
+
+		// get writer
+		PrintWriter out = response.getWriter();  
+		
+		try{ 
+			// validate if user is logged in with an account type
+			if(session.getAttribute("accountType")==null){
+				try{
+					if(request.getParameter("emailInput")!=null){
+						String emailInput = request.getParameter("emailInput");
+						String passwordInput = request.getParameter("passwordInput");
+						
+						Boolean isAdmin = false;
+						Boolean isMember = false;
+						
+						// get all users
+						ArrayList<User> UsersArray = UserUtils.getUsers();
+						
+						for (int x=0; x<UsersArray.size(); x++) {
+							String userEmail = UsersArray.get(x).getEmail();
+							String userPassword = UsersArray.get(x).getPassword();
+							String userRole = UsersArray.get(x).getUserRole();
+							
+							// checks if inputs equals details in users table
+							if(userEmail.equals(emailInput) && userPassword.equals(passwordInput)){
+								int userId = UsersArray.get(x).getUserId();
+								String username = UsersArray.get(x).getUsername();
+								
+								if(userRole.equals("Admin")){
+									isAdmin = true;
+									session.setAttribute("accountType", "admin");
+								}else if(userRole.equals("Member")){
+									isMember = true;
+									session.setAttribute("accountType", "member");
+								}
+								
+								session.setAttribute("userId", userId);
+				        		session.setAttribute("username", username);
+								response.sendRedirect("Assignment/website/index.jsp");
+							}
 						}
 						
-						session.setAttribute("userId", userId);
-		        		session.setAttribute("username", username);
-						response.sendRedirect("Assignment/website/index.jsp");
+						if(isAdmin == false && isMember == false){
+							response.sendRedirect("Assignment/website/login.jsp?accountType=none");
+						}
+						
+					}else{
+						response.sendRedirect("login.jsp");
+						System.out.println("(LoginServlet) Error: Wrong Flow\n");
 					}
+				}catch(Exception e){
+					System.out.println("(LoginServlet) Error: " + e + "\n");
 				}
-				
-				if(isAdmin == false && isMember == false){
-					response.sendRedirect("Assignment/website/login.jsp?accountType=none");
-				}
-				conn.close();
-			}else{
-				response.sendRedirect("login.jsp");
-				System.out.println("(LoginServlet) Error: Wrong Flow\n");
+			} else{
+				out.println("<script type='text/javascript'>");
+				out.println("window.location.href='Assignment/website/index.jsp';");
+				out.println("alert('You are already logged in.');");
+				out.println("</script>");
 			}
-		}catch(Exception e){
-			System.out.println("(LoginServlet) Error: " + e + "\n");
+		} catch (Exception e){
+			System.out.println("(LoginServlet) Admin Validation Error: " + e + "\n");
 		}
 	}
-
 }
