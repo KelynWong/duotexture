f5948f3da01587ebbf3dd7d536b08565c4e787b1
renfred <renfred.19@ichat.sp.edu.sg>
2020-07-30 17:20:48 +0800

    Change connection to call connection.java

--- src/servlets/AddCategoryServlet.java
+++ src/servlets/AddCategoryServlet.java
@@ -9,11 +9,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import database.connection;
+
+import java.io.PrintWriter;
+
 import java.sql.*;
 
 /**
  * Servlet implementation class AddCategoryServlet
- * Servlet implementation class EditProductServlet
+ * Servlet implementation class AddCategoryServlet
  * 
  * Class: DIT/FT/2B/21
  * Group: 1
@@ -42,82 +46,101 @@ public class AddCategoryServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		// get current session
+		HttpSession session=request.getSession();
+
+		try{ 
+			// validate if user is logged in with an account type
+			if(session.getAttribute("accountType")!=null){
+				// validate if user executing request is admin
+				if(!session.getAttribute("accountType").equals("admin")){
+					PrintWriter out = response.getWriter();  
+					out.println("<script type='text/javascript'>");
+					out.println("window.location.href='Assignment/website/index.jsp';");
+					out.println("alert('You do not have access rights.');");
+					out.println("</script>");
+
+				}
+			}else{
+				PrintWriter out = response.getWriter();  
+				out.println("<script type='text/javascript'>");
+				out.println("window.location.href='Assignment/website/index.jsp';");
+				out.println("alert('You do not have access rights.');");
+				out.println("</script>");
+			}
+		} catch (Exception e){
+			System.out.println("(AddCategoryServlet) Admin Validation Error: " + e + "\n");
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// doGet(request, response);
-
 		// get current session
 		HttpSession session=request.getSession();
 
-//		try{ 
-//			// validate if user executing request is admin
-//			if(session.getAttribute("accountType")!=null){
-//				if(!session.getAttribute("accountType").equals("admin")){
-//					PrintWriter out = response.getWriter();  
-//					out.println("<script type='text/javascript'>");
-//					out.println("window.location.href='index.jsp';");
-//					out.println("alert('You do not have access rights.');");
-//					out.println("</script>");
-//
-//				}
-//			}else{
-//				PrintWriter out = response.getWriter();  
-//				out.println("<script type='text/javascript'>");
-//				out.println("window.location.href='index.jsp';");
-//				out.println("alert('You do not have access rights.');");
-//				out.println("</script>");
-//			}
-//		} catch (Exception e){
-//			System.out.println("(EditProductServlet) Admin Validation Error: " + e + "\n");
-//		}
-		
-		try {       
-			if(request.getParameter("inputCategoryName")!=null){
-				String inputCategoryName = request.getParameter("inputCategoryName");
-				String inputCategoryDescription = request.getParameter("inputCategoryDescription");
-				String inputCategoryImageUrl = request.getParameter("inputCategoryImageUrl");
-				
-				// connect to mysql database
-				Class.forName("com.mysql.jdbc.Driver"); 
-				String connURL = "jdbc:mysql://localhost/duotexture?user=root&password=password&serverTimezone=UTC";
-				Connection conn = DriverManager.getConnection(connURL);
-				
-				// insert inputs into categories
-				String addCategoryQuery = "INSERT INTO duotexture.categories(`name`, `description`, `image`) VALUES(?, ?, ?);"; 
-				PreparedStatement pstmt = conn.prepareStatement(addCategoryQuery);
-			    pstmt.setString(1, inputCategoryName);
-			    pstmt.setString(2, inputCategoryDescription);
-			    pstmt.setString(3, inputCategoryImageUrl);
-				int count = pstmt.executeUpdate(); 
-			
-				if(count > 0){
-					response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=success"); 
-				}else{
-					response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=fail");
+		try{ 
+			// validate if user is logged in with an account type
+			if(session.getAttribute("accountType")!=null){
+				// validate if user executing request is admin
+				if(!session.getAttribute("accountType").equals("admin")){
+					PrintWriter out = response.getWriter();  
+					out.println("<script type='text/javascript'>");
+					out.println("window.location.href='Assignment/website/index.jsp';");
+					out.println("alert('You do not have access rights.');");
+					out.println("</script>");
+				}else {
+					
+					try {       
+						if(request.getParameter("inputCategoryName")!=null){
+							String inputCategoryName = request.getParameter("inputCategoryName");
+							String inputCategoryDescription = request.getParameter("inputCategoryDescription");
+							String inputCategoryImageUrl = request.getParameter("inputCategoryImageUrl");
+							
+							Connection conn = connection.connectToDatabase();
+							
+							// insert inputs into categories
+							String addCategoryQuery = "INSERT INTO duotexture.categories(`name`, `description`, `image`) VALUES(?, ?, ?);"; 
+							PreparedStatement pstmt = conn.prepareStatement(addCategoryQuery);
+						    pstmt.setString(1, inputCategoryName);
+						    pstmt.setString(2, inputCategoryDescription);
+						    pstmt.setString(3, inputCategoryImageUrl);
+							int count = pstmt.executeUpdate(); 
+						
+							if(count > 0){
+								response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=success"); 
+							}else{
+								response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=fail");
+							}
+						          
+							conn.close();    
+						}else{
+							System.out.println("(AddCategoryServlet) Error: Wrong Flow\n");
+							response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=fail");
+						}
+					} catch(java.sql.SQLIntegrityConstraintViolationException e){
+						System.out.println("(AddCategoryServlet) Error: Duplicate Entry\n");
+						response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=fail");
+					} catch (java.lang.NumberFormatException e) {         
+						System.out.println(" (AddCategoryServlet) Error: Invalid Inputs\n"); 
+						response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=fail");
+					} catch (Exception e) {         
+						System.out.println(" (AddCategoryServlet) Error: " + e + "\n"); 
+						response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=fail");
+					}
+					
 				}
-			          
-				conn.close();    
 			}else{
-				System.out.println("(AddCategoryServlet) Error: Wrong Flow\n");
-				response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=fail");
+				PrintWriter out = response.getWriter();  
+				out.println("<script type='text/javascript'>");
+				out.println("window.location.href='Assignment/website/index.jsp';");
+				out.println("alert('You do not have access rights.');");
+				out.println("</script>");
 			}
-		} catch(java.sql.SQLIntegrityConstraintViolationException e){
-			System.out.println("(AddCategoryServlet) Error: Duplicate Entry\n");
-			response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=fail");
-		} catch (java.lang.NumberFormatException e) {         
-			System.out.println(" (AddCategoryServlet) Error: Invalid Inputs\n"); 
-			response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=fail");
-		} catch (Exception e) {         
-			System.out.println(" (AddCategoryServlet) Error: " + e + "\n"); 
-			response.sendRedirect("Assignment/website/add_category.jsp?categoryAddition=fail");
-		}
+		} catch (Exception e){
+			System.out.println("(AddCategoryServlet) Admin Validation Error: " + e + "\n");
+		}	
 	}
 
 }
