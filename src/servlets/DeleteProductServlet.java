8f7f6e5bedcaa8724e85995e104d27544f6d3ac8
renfred <renfred.19@ichat.sp.edu.sg>
2020-07-30 20:35:56 +0800

    Renamed DeleteProjectServlet.java to DeleteProductServlet.java

--- src/servlets/DeleteProductServlet.java
+++ src/servlets/DeleteProductServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class DeleteProjectServlet
  */
 @WebServlet("/DeleteProjectServlet")
-public class DeleteProjectServlet extends HttpServlet {
+public class DeleteProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public DeleteProjectServlet() {
+    public DeleteProductServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
