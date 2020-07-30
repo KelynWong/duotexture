abd149f82b6004194b265eee98389a61ad56018a
renfred <renfred.19@ichat.sp.edu.sg>
2020-07-30 20:50:31 +0800

    Complete DeleteProductServlet.java

--- src/servlets/DeleteProductServlet.java
+++ src/servlets/DeleteProductServlet.java
@@ -1,16 +1,32 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import utils.ProductUtils;
 
 /**
- * Servlet implementation class DeleteProjectServlet
+ * Servlet implementation class DeleteProductServlet
+ * 
+ * Class: DIT/FT/2B/21
+ * Group: 1
+ * 
+ * Name: LEE ZONG XUN RENFRED
+ * Admin Number: P1935392 
+ * 
+ * Name: WONG EN TING KELYN
+ * Admin Number: P1935800
+ * 
  */
-@WebServlet("/DeleteProjectServlet")
+
+@WebServlet("/DeleteProductServlet")
 public class DeleteProductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,16 +42,83 @@ public class DeleteProductServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		/// get current session
+		HttpSession session=request.getSession();
+
+		// get writer
+		PrintWriter out = response.getWriter();  
+		
+		try{ 
+			// validate if user is logged in with an account type
+			if(session.getAttribute("accountType")!=null){
+				// validate if user executing request is admin
+				if(!session.getAttribute("accountType").equals("admin")){
+					out.println("<script type='text/javascript'>");
+					out.println("window.location.href='Assignment/website/index.jsp';");
+					out.println("alert('You do not have access rights.');");
+					out.println("</script>");
+				} else {
+					System.out.println("(DeleteProductServlet) There's no action to be taken for GET. Redirecting to categories.jsp to select a product of a category to edit."); 
+					response.sendRedirect("Assignment/website/categories.jsp");
+				}
+			} else{
+				out.println("<script type='text/javascript'>");
+				out.println("window.location.href='Assignment/website/index.jsp';");
+				out.println("alert('You do not have access rights.');");
+				out.println("</script>");
+			}
+		} catch (Exception e){
+			System.out.println("(DeleteProductServlet) Admin Validation Error: " + e + "\n");
+		}
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// get current session
+		HttpSession session=request.getSession();
+		
+		// get writer
+		PrintWriter out = response.getWriter();  
+		
+		// pre-define variable
+		int categoryId = 0;
+		try {
+			if(session.getAttribute("categoryId")!=null){
+				if(request.getParameter("productId")!=null){
+					categoryId = (int) session.getAttribute("categoryId");
+					int productId = Integer.parseInt(request.getParameter("productId"));
+					
+					// connect to database
+					int count = ProductUtils.deleteProduct(productId); 
+					
+					if(count > 0){
+						out.println("<script type='text/javascript'>");
+						out.println("window.location.href='product_listings.jsp?categoryId=" + categoryId + "';");
+						out.println("alert('Product has successfully been deleted.');");
+						out.println("</script>");
+					}else{
+						out.println("<script type='text/javascript'>");
+						out.println("window.location.href='product_listings.jsp?categoryId=" + categoryId + "';");
+						out.println("alert('Failed to delete product.');");
+						out.println("</script>");						
+					}   
+				}else{
+					System.out.println("(DeleteProductServlet) Error: ProductId is null.\n");
+					out.println("<script type='text/javascript'>");
+					out.println("window.location.href='product_listings.jsp?categoryId=" + categoryId + "';");
+					out.println("alert('Failed to delete product.');");
+					out.println("</script>");		
+				}
+			}else{
+				System.out.println("(DeleteProductServlet) Error: CategoryId is null.\n");
+				response.sendRedirect("categories.jsp");
+			}
+		} catch (Exception e) {         
+			System.out.println("(DeleteProductServlet) Error: " + e + "\n");
+			response.sendRedirect("categories.jsp");
+		} 
 	}
 
 }
