cb1de83fb1480924dfd35dcb4e03b52370ff638c
renfred <renfred.19@ichat.sp.edu.sg>
2020-07-30 13:16:00 +0800

    Added constructors and commented dbaccess/categories.java

--- src/dbaccess/categories.java
+++ src/dbaccess/categories.java
@@ -6,28 +6,55 @@ public class categories {
 	private String description;
 	private String image;
 	
+	// default constructor
+	public categories() {
+		
+	}
+	
+	// constructor overload
+	public categories(int categoryId, String name, String description, String image) {
+		this.categoryId = categoryId;
+		this.name = name;
+		this.description = description;
+		this.image = image;
+	}
+	
+	// get category id
 	public int getCategoryId() {
 		return categoryId;
 	}
+	
+	// get category name
 	public String getName() {
 		return name;
 	}
+	
+	// get category description
 	public String getDescription() {
 		return description;
 	}
+	
+	// get category image
 	public String getImage() {
 		return image;
 	}
 	
+	// set category id (should not be in usage)
 	public void setCategoryId(int categoryId) {
 		this.categoryId= categoryId;
 	}
+	
+	// set category name
 	public void setName(String name) {
 		this.name= name;
 	}
+	
+	// set category description
 	public void setDescription(String description) {
 		this.description= description;
 	}
+	
+	// set category image
 	public void setImage(String image) {
 		this.image= image;
 	}
