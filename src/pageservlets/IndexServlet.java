fba7f8d0c910e9ada9792522716948422cafb90f
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-03 21:01:57 +0800

    Completed IndexServlet.java

--- src/pageservlets/IndexServlet.java
+++ src/pageservlets/IndexServlet.java
@@ -52,9 +52,53 @@ public class IndexServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// forward request to jsp for display
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher("Assignment/website/index.jsp");
-		requestDispatcher.forward(request, response);
+		try {
+			// declare client
+			Client client = ClientBuilder.newClient();
+			
+			// target java and parse in data
+			WebTarget target = client.target("http://localhost:8080/ST0510-JAD-Assignment/api/")
+					.path("categoryservices/getcategories");
+			
+			// declare media is an application/json
+			Invocation.Builder invoBuilder = target.request(MediaType.APPLICATION_JSON);
+			
+			// get response
+			Response resp = invoBuilder.get();
+			
+			// if response status is ok
+			if(resp.getStatus() == Response.Status.OK.getStatusCode()) {
+				// get results from java class
+				JSONArray categoriesJSONArray = new JSONArray(resp.readEntity(new GenericType<String>() {}));
+				ArrayList<Category> categoriesArrayList = new ArrayList<Category>();
+				
+				for(int x=0; x<categoriesJSONArray.length(); x++) {
+					JSONObject categoryObject = (JSONObject) categoriesJSONArray.get(x);
+					
+					int categoryId = categoryObject.getInt("categoryId");
+					String name = categoryObject.getString("name");
+					String description = categoryObject.getString("description");
+					String image = categoryObject.getString("image");
+					
+					Category category = new Category(categoryId, name, description, image);
+					categoriesArrayList.add(category);
+				}
+				
+				// store in request
+				request.setAttribute("categoriesArrayList", categoriesArrayList);
+				
+				// forward request to jsp for display
+				RequestDispatcher requestDispatcher = request.getRequestDispatcher("Assignment/website/index.jsp");
+				requestDispatcher.forward(request, response);
+			} else {
+				System.out.println("(CategoriesServlet.jsp) Error: Response not ok. \n");
+				response.sendRedirect("Assignment/website/index.jsp");
+			}
+			
+		} catch (Exception e) {
+			System.out.println("(CategoriesServlet.jsp) Error: " + e + "\n");
+			response.sendRedirect("Assignment/website/index.jsp");
+		}
 
 	}
 
