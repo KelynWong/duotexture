9650e54a5bc15515eeb3f9232127bed1f0f9002d
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-04 14:06:52 +0800

    Added path /memberservices/getmemberbyuserid (MemberServices.java)

--- src/services/MemberServices.java
+++ src/services/MemberServices.java
@@ -12,7 +12,9 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javabeans.Member;
+import javabeans.Order;
 import utils.MemberUtils;
+import utils.OrderUtils;
 
 @Path("memberservices")
 public class MemberServices {
@@ -50,10 +52,34 @@ public class MemberServices {
 	@Path("getmemberbyid")
 	@GET
 	@Produces("application/json")
-	public Response getMemberById(@QueryParam("memberId") int memberId) {
+	public Response getMemberById(@QueryParam("userId") int userId) {
 		
 		try {
-			Member memberBean = MemberUtils.getMemberById(memberId);
+			Member memberBean = MemberUtils.getMemberByUserId(userId);
+			
+			JSONObject memberObject = new JSONObject();
+
+			memberObject.put("userId", memberBean.getUserId());
+			memberObject.put("first_name", memberBean.getFirstName());
+			memberObject.put("last_name", memberBean.getLastName());
+			memberObject.put("country", memberBean.getCountry());
+			memberObject.put("address", memberBean.getAddress());
+			memberObject.put("postal_code", memberBean.getPostalCode());
+			
+			return Response.status(200).entity(memberObject.toString()).build();
+			
+		} catch (Exception e) {
+			return Response.status(200).entity("Error occurred.").build();
+		}
+	}
+	
+	@Path("getmemberbyuserid")
+	@GET
+	@Produces("application/json")
+	public Response getMemberByUserId(@QueryParam("userId") int userId) {
+		
+		try {
+			Member memberBean = MemberUtils.getMemberById(userId);
 			
 			JSONObject memberObject = new JSONObject();
 
