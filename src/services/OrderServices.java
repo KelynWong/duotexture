4d86530b7ba6dcd6754f3a561f638fa36e8f5af1
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-08 22:17:14 +0800

    Commented OrderServices.java

--- src/services/OrderServices.java
+++ src/services/OrderServices.java
@@ -17,16 +17,23 @@ import utils.OrderUtils;
 @Path("orderservices")
 public class OrderServices {
 	
+	// get all orders
 	@Path("getorders")
 	@GET
 	@Produces("application/json")
 	public Response getOrders() {
 		
+		// create JSONArray
 		JSONArray ordersJSONArray = new JSONArray();
+		
+		// declare ArrayList<Order> variable
 		ArrayList<Order> ordersArrayList;
 		
 		try {
+			// get ArrayList result from utils and store it into ordersArrayList
 			ordersArrayList = OrderUtils.getOrders();
+			
+			// convert result in ordersArrayList to JSONObject and store in ordersJSONArray
 			for (int x=0; x<ordersArrayList.size(); x++) {
 				JSONObject orderObject = new JSONObject();
 				
@@ -37,23 +44,31 @@ public class OrderServices {
 				ordersJSONArray.put(orderObject);
 			}
 			
+			// return success data
 			return Response.status(200).entity(ordersJSONArray.toString()).build();
 			
 		} catch (Exception e) {
+			// return error data
 			return Response.status(200).entity("Error occurred.").build();
 		}
 	}
 	
+	// get orders by user id
 	@Path("getordersbyuserid")
 	@GET
 	@Produces("application/json")
 	public Response getOrdersByUserId(@QueryParam("userId") int userId) {
 		
+		// create JSONArray
 		JSONArray ordersJSONArray = new JSONArray();
+		// declare ArrayList<Order> variable
 		ArrayList<Order> ordersArrayList;
 		
 		try {
+			// get ArrayList result from utils and store it into ordersArrayList
 			ordersArrayList = OrderUtils.getOrdersByUserId(userId);
+			
+			// convert result in ordersArrayList to JSONObject and store in ordersJSONArray
 			for (int x=0; x<ordersArrayList.size(); x++) {
 				JSONObject orderObject = new JSONObject();
 				
@@ -64,9 +79,11 @@ public class OrderServices {
 				ordersJSONArray.put(orderObject);
 			}
 			
+			// return success data
 			return Response.status(200).entity(ordersJSONArray.toString()).build();
 			
 		} catch (Exception e) {
+			// return error data	
 			return Response.status(200).entity("Error occurred.").build();
 		}
 	}
