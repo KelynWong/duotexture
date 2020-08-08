4900d5a52538375aff6b1e8c3eed87fbe38b63d7
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-08 21:57:46 +0800

    Commented CardServices.java

--- src/services/CardServices.java
+++ src/services/CardServices.java
@@ -17,16 +17,23 @@ import utils.CardUtils;
 @Path("cardservices")
 public class CardServices {
 	
+	// get all cards
 	@Path("getcards")
 	@GET
 	@Produces("application/json")
 	public Response getCards() {
 		
+		// create JSONArray
 		JSONArray cardsJSONArray = new JSONArray();
+		
+		// declare ArrayList<Card> variable
 		ArrayList<Card> cardsArrayList;
 		
 		try {
+			// get ArrayList result from utils and store it into cardsArrayList
 			cardsArrayList = CardUtils.getCards();
+			
+			// convert result in cardsArrayList to JSONObject and store in cardsJSONArray
 			for (int x=0; x<cardsArrayList.size(); x++) {
 				JSONObject cardObject = new JSONObject();
 				
@@ -40,21 +47,26 @@ public class CardServices {
 				cardsJSONArray.put(cardObject);
 			}
 			
+			// return success data
 			return Response.status(200).entity(cardsJSONArray.toString()).build();
 			
 		} catch (Exception e) {
+			// return error data
 			return Response.status(200).entity("Error occurred.").build();
 		}
 	}
 	
+	// get card by user id
 	@Path("getcardbyuserid")
 	@GET
 	@Produces("application/json")
 	public Response getCardByUserId(@QueryParam("userId") int userId) {
 		
 		try {
+			// get Card result from utils and store it into cardBean
 			Card cardBean = CardUtils.getCardByUserId(userId);
 			
+			// store inside JSONObject
 			JSONObject cardObject = new JSONObject();
 
 			cardObject.put("userId", cardBean.getUserId());
@@ -64,9 +76,11 @@ public class CardServices {
 			cardObject.put("expiryYear", cardBean.getExpiryYear());
 			cardObject.put("cvv", cardBean.getCvv());
 			
+			// return success data
 			return Response.status(200).entity(cardObject.toString()).build();
 			
 		} catch (Exception e) {
+			// return error data
 			return Response.status(200).entity("Error occurred.").build();
 		}
 	}
