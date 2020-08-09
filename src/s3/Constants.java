a6fa7ce72df3d3d8177784dca881084dd692bef4
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-09 19:05:42 +0800

    Delete Constants.java

--- src/s3/Constants.java
+++
@@ -1,7 +0,0 @@
-package s3;
-
-public class Constants {
-	final static String ACCESS_KEY_ID = "AKIA3BFIE64ZPYIK2TGY";
-	
-	final static String ACCESS_SEC_KEY = "MbSuvs0l1/o3J0RMNkR0eOxvElynukO6mIRNLnr+";
-}
