c33e9b90c00e432564a4034beda86221d29b4bf7
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-09 19:17:41 +0800

    Removed specific thrown exception in CreateBucket() (S3Utils.java)

--- src/utils/S3Utils.java
+++ src/utils/S3Utils.java
@@ -1,12 +1,11 @@
 package utils;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.AmazonS3Exception;
 
 import connection.S3Client;
 
 public class S3Utils {
-	public static void CreateBucket() throws AmazonS3Exception, Exception {
+	public static void CreateBucket() throws Exception {
 		AmazonS3 s3client = S3Client.CreateS3Client();
 		String bucket_name = "jad-bucket";
 		
