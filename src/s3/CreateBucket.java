039dd1b13abb2c5b1e32b5d56bb889bd1fdeb474
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-09 19:09:36 +0800

    Added Catch Exception in CreateBucket.java

--- src/s3/CreateBucket.java
+++ src/s3/CreateBucket.java
@@ -1,30 +1,18 @@
 package s3;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 
 public class CreateBucket {
 	public static void main(String[] args) {
-		
-		// pre-define variables
-		final String ACCESS_KEY_ID = "AKIA3BFIE64ZPYIK2TGY";
-		final String ACCESS_SEC_KEY = "MbSuvs0l1/o3J0RMNkR0eOxvElynukO6mIRNLnr+";
-		
-		// credentials object identifying user for authentication (user are given AmazonS3FullAccess)
-		AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY_ID, ACCESS_SEC_KEY);
-		
-		// create a client connection based on credentials
-		AmazonS3 s3client = AmazonS3ClientBuilder.standard() .withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_EAST_2).build();;
-		String bucket_name = "jad-bucket";
-		
 		try {
+			AmazonS3 s3client = S3Client.CreateS3Client();
+			String bucket_name = "jad-bucket";
+			
 			s3client.createBucket(bucket_name);
 		} catch (AmazonS3Exception e) {
+			System.out.println("(CreateBucket.java) AmazonS3Exception Error: " + e + "\n");
+		} catch (Exception e) {
 			System.out.println("(CreateBucket.java) Error: " + e + "\n");
 		}
 	}
