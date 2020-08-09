9c7a0e90157b0e06507e23c00aa0fda84f45bf40
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-09 19:19:56 +0800

    Completed ListS3Buckets() in S3Utils.java

--- src/utils/S3Utils.java
+++ src/utils/S3Utils.java
@@ -1,6 +1,9 @@
 package utils;
 
+import java.util.List;
+
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.Bucket;
 
 import connection.S3Client;
 
@@ -10,5 +13,11 @@ public class S3Utils {
 		String bucket_name = "jad-bucket";
 		
 		s3client.createBucket(bucket_name);
+	}
+	
+	public static List<Bucket> ListS3Buckets() throws Exception {
+		AmazonS3 s3client = S3Client.CreateS3Client();
+		List<Bucket> buckets = s3client.listBuckets();
+		return buckets;
 	}
 }
