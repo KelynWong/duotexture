85b5e92cf6898c18d48d14e47b369dd69088be8e
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-09 19:20:48 +0800

    Renamed methods in S3Utils.java

--- src/utils/S3Utils.java
+++ src/utils/S3Utils.java
@@ -8,14 +8,14 @@ import com.amazonaws.services.s3.model.Bucket;
 import connection.S3Client;
 
 public class S3Utils {
-	public static void CreateBucket() throws Exception {
+	public static void createBucket() throws Exception {
 		AmazonS3 s3client = S3Client.CreateS3Client();
 		String bucket_name = "jad-bucket";
 		
 		s3client.createBucket(bucket_name);
 	}
 	
-	public static List<Bucket> ListS3Buckets() throws Exception {
+	public static List<Bucket> listS3Buckets() throws Exception {
 		AmazonS3 s3client = S3Client.CreateS3Client();
 		List<Bucket> buckets = s3client.listBuckets();
 		return buckets;
