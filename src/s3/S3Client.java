581282062effc9f3cc4aefd7e9547eab086a6830
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-09 19:05:22 +0800

    Completed S3Client.java

---
+++ src/s3/S3Client.java
@@ -0,0 +1,25 @@
+package s3;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+public class S3Client {
+	
+	public static AmazonS3 CreateS3Client() {
+		// pre-define variables
+		final String ACCESS_KEY_ID = "AKIA3BFIE64ZPYIK2TGY";
+		final String ACCESS_SEC_KEY = "MbSuvs0l1/o3J0RMNkR0eOxvElynukO6mIRNLnr+";
+		
+		// credentials object identifying user for authentication (user are given AmazonS3FullAccess)
+		AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY_ID, ACCESS_SEC_KEY);
+		
+		// create a client connection based on credentials
+		AmazonS3 s3client = AmazonS3ClientBuilder.standard() .withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.US_EAST_2).build();;
+		
+		return s3client;
+	}
+}
