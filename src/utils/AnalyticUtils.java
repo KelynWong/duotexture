60b5cf7679ec815b26afb9c4746db67ff87f28bb
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-13 15:11:16 +0800

    Attempt at getMembers in AnalyticsUtils

--- src/utils/AnalyticUtils.java
+++ src/utils/AnalyticUtils.java
@@ -1,5 +1,49 @@
 package utils;
 
-public class AnalyticUtils {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import connection.Database;
+import javabeans.Member;
+
+public class AnalyticUtils {
+	
+	// get all members
+	public static ArrayList<Member> getMembers (int count) throws SQLException, ClassNotFoundException {
+		// connect to database
+		Connection conn = Database.connectToDatabase();
+		
+		int limit = count*5;
+		// prepared statement, get all member query and result
+		String getMembersQuery = "SELECT * FROM duotexture.members LIMIT ?, 5;";
+		PreparedStatement pstmt = conn.prepareStatement(getMembersQuery);
+		pstmt.setInt(1, limit);
+		ResultSet getMembersResult = pstmt.executeQuery();
+		
+		// create new ArrayList of Members
+		ArrayList<Member> membersArrayList = new ArrayList<Member>();
+		
+		// loop if there are new row
+		while(getMembersResult.next()) {
+			// create an instance of Member
+			Member MemberBean = new Member();
+			
+			MemberBean.setUserId(getMembersResult.getInt("userId"));
+			MemberBean.setFirstName(getMembersResult.getString("first_name"));
+			MemberBean.setLastName(getMembersResult.getString("last_name"));
+			MemberBean.setCountry(getMembersResult.getString("country"));
+			MemberBean.setAddress(getMembersResult.getString("address"));
+			MemberBean.setPostalCode(getMembersResult.getString("postal_code"));
+			
+			// add MembersBean to categoriesArrayList
+			membersArrayList.add(MemberBean);
+		}
+		
+		// close connection
+		conn.close();
+		return membersArrayList;
+	}
 }
