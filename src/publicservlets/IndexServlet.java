94c5334fc410e7d50ac4e047712dce514679327e
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-13 15:17:07 +0800

    Success in making IndexServlet the default url /

--- src/publicservlets/IndexServlet.java
+++ src/publicservlets/IndexServlet.java
@@ -36,7 +36,7 @@ import javabeans.Category;
  * 
  */
 
-@WebServlet("/index")
+@WebServlet(urlPatterns = {"/index", "/"})
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
