d03382917f4653af09f9d15b59b3eb3131053eef
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-15 19:13:48 +0800

    Completed AnalyticsUtils

--- src/utils/AnalyticUtils.java
+++ src/utils/AnalyticUtils.java
@@ -10,6 +10,19 @@ import connection.Database;
 import javabeans.Member;
 import javabeans.Product;
 
+/**
+ * 
+ * Class: DIT/FT/2B/21
+ * Group: 1
+ * 
+ * Name: LEE ZONG XUN RENFRED
+ * Admin Number: P1935392 
+ * 
+ * Name: WONG EN TING KELYN
+ * Admin Number: P1935800
+ * 
+ */
+
 public class AnalyticUtils {
 	
 	// get all members
@@ -20,10 +33,10 @@ public class AnalyticUtils {
 		// compute limit formula
 		int limit = count*5;
 		
-		// prepared statement, get all member query and result
+		// prepared statement, get all members query and result
 		String getMembersQuery = "SELECT * FROM duotexture.members WHERE members.country LIKE ? OR members.address LIKE ? ORDER BY members.userId ";
 		
-		// prepared statement inserts string, which is denied, therefore validation required
+		// prepared statement inserts string, which is denied for ORDER BY, therefore if else validation required
 		if(order.equals("ASC")) {
 			getMembersQuery += "ASC LIMIT ?,5;";
 		}else {
@@ -51,7 +64,7 @@ public class AnalyticUtils {
 			MemberBean.setAddress(getMembersResult.getString("address"));
 			MemberBean.setPostalCode(getMembersResult.getString("postal_code"));
 			
-			// add MembersBean to membersArrayList
+			// add MemberBean to membersArrayList
 			membersArrayList.add(MemberBean);
 		}
 		
@@ -61,52 +74,52 @@ public class AnalyticUtils {
 	}
 	
 	// get all products
-		public static ArrayList<Product> getProducts (int count, String keyword, String order) throws SQLException, ClassNotFoundException {
-			// connect to database
-			Connection conn = Database.connectToDatabase();
-			
-			// compute limit formula
-			int limit = count*5;
-			
-			// prepared statement, get all products query and result
-			String getProductsQuery = "SELECT * FROM duotexture.products WHERE products.name LIKE ? OR products.description LIKE ? ORDER BY products.productId ";
-			
-			// prepared statement inserts string, which is denied, therefore validation required
-			if(order.equals("ASC")) {
-				getProductsQuery += "ASC LIMIT ?,5;";
-			}else {
-				getProductsQuery += "DESC LIMIT ?,5;";
-			}
-			
-			PreparedStatement pstmt = conn.prepareStatement(getProductsQuery);
-			pstmt.setString(1, "%" + keyword + "%");
-			pstmt.setString(2, "%" + keyword + "%");
-			pstmt.setInt(3, limit);
-			ResultSet getProductsResult = pstmt.executeQuery();
-			
-			// create new ArrayList of Product
-			ArrayList<Product> productArrayList = new ArrayList<Product>();
-			
-			// loop if there are new row
-			while(getProductsResult.next()) {
-				// create an instance of Product
-				Product productBean = new Product();
-				
-				productBean.setProductId(getProductsResult.getInt("productId"));
-				productBean.setName(getProductsResult.getString("name"));
-				productBean.setDescription(getProductsResult.getString("description"));
-				productBean.setCostPrice(getProductsResult.getDouble("cost_price"));
-				productBean.setRetailPrice(getProductsResult.getDouble("retail_price"));
-				productBean.setQuantity(getProductsResult.getInt("quantity"));
-				productBean.setCategoryId(getProductsResult.getInt("categoryId"));
-				productBean.setImage(getProductsResult.getString("image"));
-				
-				// add ProductBean to productArrayList
-				productArrayList.add(productBean);
-			}
-			
-			// close connection
-			conn.close();
-			return productArrayList;
+	public static ArrayList<Product> getProducts (int count, String keyword, String order) throws SQLException, ClassNotFoundException {
+		// connect to database
+		Connection conn = Database.connectToDatabase();
+		
+		// compute limit formula
+		int limit = count*5;
+		
+		// prepared statement, get all products query and result
+		String getProductsQuery = "SELECT * FROM duotexture.products WHERE products.name LIKE ? OR products.description LIKE ? ORDER BY products.productId ";
+		
+		// prepared statement inserts string, which is denied for ORDER BY, therefore if else validation required
+		if(order.equals("ASC")) {
+			getProductsQuery += "ASC LIMIT ?,5;";
+		}else {
+			getProductsQuery += "DESC LIMIT ?,5;";
 		}
+		
+		PreparedStatement pstmt = conn.prepareStatement(getProductsQuery);
+		pstmt.setString(1, "%" + keyword + "%");
+		pstmt.setString(2, "%" + keyword + "%");
+		pstmt.setInt(3, limit);
+		ResultSet getProductsResult = pstmt.executeQuery();
+		
+		// create new ArrayList of Product
+		ArrayList<Product> productArrayList = new ArrayList<Product>();
+		
+		// loop if there are new row
+		while(getProductsResult.next()) {
+			// create an instance of Product
+			Product productBean = new Product();
+			
+			productBean.setProductId(getProductsResult.getInt("productId"));
+			productBean.setName(getProductsResult.getString("name"));
+			productBean.setDescription(getProductsResult.getString("description"));
+			productBean.setCostPrice(getProductsResult.getDouble("cost_price"));
+			productBean.setRetailPrice(getProductsResult.getDouble("retail_price"));
+			productBean.setQuantity(getProductsResult.getInt("quantity"));
+			productBean.setCategoryId(getProductsResult.getInt("categoryId"));
+			productBean.setImage(getProductsResult.getString("image"));
+			
+			// add ProductBean to productArrayList
+			productArrayList.add(productBean);
+		}
+		
+		// close connection
+		conn.close();
+		return productArrayList;
+	}
 }
