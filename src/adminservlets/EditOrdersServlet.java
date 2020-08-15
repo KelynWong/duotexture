82e05b5af705c896efd4cb0dc3047a9811d4138d
renfred <renfred.19@ichat.sp.edu.sg>
2020-08-15 22:52:13 +0800

    Completed EditOrdersServlet

--- src/adminservlets/EditOrdersServlet.java
+++ src/adminservlets/EditOrdersServlet.java
@@ -2,12 +2,7 @@ package adminservlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.regex.Pattern;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,32 +10,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import javabeans.Cart;
 import javabeans.Order;
-import utils.CardUtils;
-import utils.CartUtils;
 import utils.OrderUtils;
 import utils.PurchaseUtils;
 
 /**
  * Servlet implementation class EditOrdersServlet
+ * 
+ * Class: DIT/FT/2B/21
+ * Group: 1
+ * 
+ * Name: LEE ZONG XUN RENFRED
+ * Admin Number: P1935392 
+ * 
+ * Name: WONG EN TING KELYN
+ * Admin Number: P1935800
+ * 
  */
+
 @WebServlet("/EditOrdersServlet")
 public class EditOrdersServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EditOrdersServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    /* Get Method */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		// get current session
 		HttpSession session=request.getSession();
 
@@ -71,10 +65,9 @@ public class EditOrdersServlet extends HttpServlet {
 		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	/* Post Method */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		// get current session
 		HttpSession session=request.getSession();
 						
@@ -93,45 +86,44 @@ public class EditOrdersServlet extends HttpServlet {
 				} else {	
 					int userId = (int)session.getAttribute("userId");
 					ArrayList<Order> orderArrayList = new ArrayList<Order>();
+					
+					// get all orders by user id
 					orderArrayList = OrderUtils.getOrdersByUserId(userId);
-									
+					
 					if(orderArrayList.size() != 0) {
-						int count = 0;
-						// transfer to purchase
+						int insertPurchaseCount = 0;
+						
+						// transfer every order to purchase
 						for (int x=0; x<orderArrayList.size(); x++) {
-							// get current date
-							DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		    				String dateTime = dateFormat.format(new Date());
-		    				
 		    				// add purchase
-							count = PurchaseUtils.insertPurchase(orderArrayList.get(x).getUserId(), orderArrayList.get(x).getProductId(), orderArrayList.get(x).getQuantity(), dateTime);
+							insertPurchaseCount = PurchaseUtils.insertPurchase(orderArrayList.get(x).getUserId(), orderArrayList.get(x).getProductId(), orderArrayList.get(x).getQuantity(), orderArrayList.get(x).getDateTime());
 						}
 										
-						if(count > 0) {
+						if(insertPurchaseCount > 0) {
 							// delete order
-							int count2 = OrderUtils.deleteOrder(userId); 
+							int deleteOrderCount = OrderUtils.deleteOrder(userId); 
 											
-							if(count2 > 0) {
+							if(deleteOrderCount > 0) {
 								out.println("<script type='text/javascript'>");
 								out.println("window.location.href='../ST0510-JAD-Assignment/orders';");
 								out.println("alert('Items have be confirmed that it is delivered!');");
 								out.println("</script>");
-							}else {
-								System.out.println("(adminservlets/EditOrdersServlet) Error: Failed to delete order\n");
+							} else {
+								System.out.println("(adminservlets/EditOrdersServlet) Error: Failed to delete order \n");
 								response.sendRedirect(request.getContextPath() + "/purchases");
 							}
-						}else {
-							System.out.println("(adminservlets/EditOrdersServlet) Error: Failed to insert to purchases\n");
+						} else {
+							System.out.println("(adminservlets/EditOrdersServlet) Error: Failed to insert to purchases \n");
 							response.sendRedirect(request.getContextPath() + "/purchases");
 						}
-					}else {
+					} else {
 						out.println("<script type='text/javascript'>");
 						out.println("window.location.href='../ST0510-JAD-Assignment/orders';");
 						out.println("alert('You have no orders!');");
 						out.println("</script>");
 					}	
 				}
-			}else{
+			} else{
 				out.println("<script type='text/javascript'>");
 				out.println("window.location.href='Assignment/website/index.jsp';");
 				out.println("alert('You do not have access rights.');");
